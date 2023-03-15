532c3ebf96cfdf914618ee1783ed84ae09589ac5
Shola Onoriemu <pyshola@gmail.com>
2023-03-15 17:27:25 +0100

    list table

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- remove hbtn_0c_0 from database
+-- remove hbtn_0c_0 from database if exist
+REMOVE DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- List all table from database
+-- Query list of table from mysql
+SHOW TABLES;
