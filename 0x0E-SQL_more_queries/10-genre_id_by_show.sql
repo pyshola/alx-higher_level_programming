23c5ca8d5cc1693c4946943b4950f6bdb95e0f0b
Shola Onoriemu <pyshola@gmail.com>
2023-03-15 18:44:10 +0100

    query

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Script that lists all cities contained in the database
+-- Query to join cities and states
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
