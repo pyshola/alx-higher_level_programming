cabadffda0bd0bde5307142d9806678128103f1e
Shola Onoriemu <pyshola@gmail.com>
2023-05-24 23:56:48 +0000

    web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,31 +1,33 @@
 #!/usr/bin/node
+
+function doRequest(url){
+  return new Promise(function (resolve, reject){
+   request(url, function(error, response, body){
+    if(error == null){
+      resolve(body)
+    }
+    else{
+     reject(error)
+    }
+  });
+ });
+}
+
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-let id = parseInt(process.argv[2], 10);
-let characters = [];
 
-
-request(url, function (err, response, body) {
+request(url, async function (err, response, body) {
   if (err == null) {
     const resp = JSON.parse(body);
-    const results = resp.results;
-    if (id < 4) {
-      id += 3;
-    } else {
-      id -= 3;
-    }
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === id) {
-        characters = results[i].characters;
-        break;
-      }
-    }
+    const characters = resp.characters; 
     for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+     try{
+       let res =  await doRequest(characters[j]);
+       console.log(JSON.parse(res).name);
+     }
+     catch(error){
+       console.log(error);
+     }
+    };
   }
 });
